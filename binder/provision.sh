8354d4fc7bc6450a706cd9fcf8d1926415b6a0d2
Scott Collis <scollis.acrf@gmail.com>
2019-10-29 15:12:55 -0500

    ADD: Binder and Pangeo setup

---
+++ binder/provision.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cd ~
+mkdir data
+cd data
+mkdir cp2
+cd cp2
+wget http://mcs.anl.gov/~scollis/cp2.tgz
+tar -xvzf cp2.tgz
+rm cp2.tgz
+cd ..
+mkdir arm
+cd arm
+wget http://www.mcs.anl.gov/~scollis/pyart/csapr_test_case.nc
+wget http://www.mcs.anl.gov/~scollis/pyart/nsaxsaprppiC1.a1.20140201.184802.nc
+wget http://www.mcs.anl.gov/~scollis/pyart/sgpxsaprcmacsurI5.c1.20170801.044013.nc
+cd ..
+cd ~
