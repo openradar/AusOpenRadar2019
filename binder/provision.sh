ab08dc5505576cbb833a51fced03738208748635
Scott Collis <scollis.acrf@gmail.com>
2019-11-02 09:05:43 -0500

    ADD: direct download of met data

--- binder/provision.sh
+++ binder/provision.sh
@@ -19,4 +19,6 @@ wget http://www.mcs.anl.gov/~scollis/pyart/csapr_test_case.nc
 wget http://www.mcs.anl.gov/~scollis/pyart/nsaxsaprppiC1.a1.20140201.184802.nc
 wget http://www.mcs.anl.gov/~scollis/pyart/sgpxsaprcmacsurI5.c1.20170801.044013.nc
 cd ..
+wget http://www.mcs.anl.gov/~scollis/metdata.tgz
+tar -xvzf metdata.tgz
 cd ~
